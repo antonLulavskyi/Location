f57de88d3daf2d1cc3dc6440e5f8a9fc9e4a0196
Anton Liuliavskyi <antonliuliawskyi@gmail.com>
2019-04-12 11:54:03 +0200

    Initial Commit

---
+++ Location/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Location
+//
+//  Created by Test on 12/04/2019.
+//  Copyright © 2019 Peanch. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Location/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Location
+//
+//  Created by Test on 12/04/2019.
+//  Copyright © 2019 Peanch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
